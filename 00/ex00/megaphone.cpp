f0b828bcabdc80b52a417342a5f7de5aea653bb4
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-09-01 16:31:22 +0200

    tests

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mvautrot <mvautrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 15:55:25 by mvautrot          #+#    #+#             */
-/*   Updated: 2023/09/01 16:07:50 by mvautrot         ###   ########.fr       */
+/*   Updated: 2023/09/01 16:31:02 by mvautrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int main(void)
 {
-	std::cout << "BONJOUR";
+	std::cout << "Bonjour," << std::endl;
+	std::cout << "ceci est un test," << std::endl << "ayant pour objectif de comprendre les bases du c++." << std::endl;
+	return 0;
 }
