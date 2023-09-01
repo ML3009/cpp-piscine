32ec9ab88580ccb3ca41d681f1082f6bb4898b83
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-09-01 16:08:48 +0200

    ex_00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,13 @@
 /*   By: mvautrot <mvautrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 15:55:25 by mvautrot          #+#    #+#             */
-/*   Updated: 2023/09/01 15:55:26 by mvautrot         ###   ########.fr       */
+/*   Updated: 2023/09/01 16:07:50 by mvautrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "BONJOUR";
+}
