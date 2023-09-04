46b3a1aa1f7f0f566b798b022feba18aceee37ea
Marie-Laure <vautrot.marielaure@gmail.com>
2023-09-04 08:14:20 -0700

    tests

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -10,11 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <iostream> // directive de preprocesseur-> inclut bibli iostream
+#include <string>
+//using namespace std = permet d indiquer quel lot de fonctionnalite notre fichier source va aller piocher
+// std correspond a la bibiliotheque standard livree par defaut avec le cpp et dont iostream fait partie
 
 int main(void)
 {
+	int				ageUtilisateur(16);
+	int				nombreAmis(432);
+	std::string 	nomUtilisateur("Albert");
+	int&			maVariable(ageUtilisateur);
+	
 	std::cout << "Bonjour," << std::endl;
-	std::cout << "ceci est un test," << std::endl << "ayant pour objectif de comprendre les bases du c++." << std::endl;
+	std::cout << "Votre avez " << ageUtilisateur << std::endl;
+	std::cout << "Vous avez egalement " << maVariable << " ans." << std::endl;
+	std::cout << "Vous avez " << nombreAmis << " amis et votre nom est " << nomUtilisateur << std::endl;
 	return 0;
 }
