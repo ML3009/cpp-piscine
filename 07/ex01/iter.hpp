94fff9a9d707c63ef71bf0c4655fafcd543bba18
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-11-16 17:32:11 +0100

    ex01

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -6,8 +6,27 @@
 /*   By: mvautrot <mvautrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:32:39 by mvautrot          #+#    #+#             */
-/*   Updated: 2023/11/16 16:39:22 by mvautrot         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:27:57 by mvautrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void	iter(T *tab, size_t len, void (*f)(T&)){
+	for (size_t i = 0; i < len; i++)
+		f(tab[i]);
+	return;
+}
+
+template <typename T>
+void	iter(const T *tab, const size_t len, void (*f)(const T&)){
+	for (size_t i = 0; i < len; i++)
+		f(tab[i]);
+	return;
+}
+
+
